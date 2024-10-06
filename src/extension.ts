27928c3656086488a39c436fad66dcd6021e9e58
Lucas S <lucas.sorribes@gmail.com>
2024-10-06 18:17:25 +0200

    add first implementation of status bar icon,

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "titus-vscode-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('titus-vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from titus-vscode-extension!');
-	});
+  const statusBarIcon = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+  statusBarIcon.text = `$(rocket) Extension Active`;
+  statusBarIcon.tooltip = "Your custom extension is active and ready!";
+  statusBarIcon.show(); // Display the status bar item
 
-	context.subscriptions.push(disposable);
+  const insertVariable = vscode.commands.registerCommand('titus-vscode-extension.insertVariable', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const snippetText = 'var(--TITUS-);';
+        
+        // The `$0` placeholder is where the cursor will be placed after insertion
+        const snippet = new vscode.SnippetString(snippetText.replace('-)', '-$0)'));
+
+        // Insert the snippet at the current cursor position
+        editor.insertSnippet(snippet);
+    }
+  });
+  
+  context.subscriptions.push(statusBarIcon);
+	context.subscriptions.push(insertVariable);
 }
 
 // This method is called when your extension is deactivated
